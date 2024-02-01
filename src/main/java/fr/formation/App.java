3e7c33374e443e1ceb352f2a9266b224f325adc8
Yassyn06 <labonnesource7@gmail.com>
2024-02-01 16:17:52 +0100

    first commit

---
+++ src/main/java/fr/formation/App.java
@@ -0,0 +1,18 @@
+package fr.formation;
+
+import fr.formation.services.Calcul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calcul calcul = new Calcul();
+        int resSomme = calcul.somme(2 , 3);
+        System.out.println(resSomme);
+
+    }
+}
